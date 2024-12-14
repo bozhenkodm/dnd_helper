df3e719d3d4d895127498465573b76adf0b8d88d
d.bozhenko <dbozhenko@ngenix.net>
2024-12-15 01:08:02 +0300

    Reset migrations

--- base/models/feats.py
+++ base/models/feats.py
@@ -13,9 +13,7 @@ class Feat(ConstraintAbstract):
     min_level = models.PositiveSmallIntegerField(
         verbose_name=_('Minimal level'), default=1, choices=((1, 1), (11, 11), (21, 21))
     )
-    text = models.TextField(
-        verbose_name=_('Text'), null=True, blank=True
-    )
+    text = models.TextField(verbose_name=_('Text'), null=True, blank=True)
 
     def __str__(self):
         return self.name
